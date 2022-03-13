3b60a63d53cc3805f32b4a2e034daf40f8b78ee3
MariMatos2112 <maristudies2112@gmail.com>
2022-03-13 18:48:57 -0300

    configuring canvas

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 const canvas = document.querySelector("canvas");
 
-let canvasWidth = 400;
-let canvasHeight = 400;
+let canvasWidth =  1024, canvasHeight = 512; mapMode = 'dark-v9', mapAngle = 0, mapZoom = 0, mapLagitute = 0, mapLongitude = 0
 
 const settingUpCanvas = () => {
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
 
-  canvas.style.backgroundImage = `url(https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-122.4241,37.78,15.25,0,60/${canvasWidth}x${canvasHeight}?access_token=pk.eyJ1IjoibWFyaS1hbm5lIiwiYSI6ImNsMG83amw3bTFuaTQzaWw0Z2w1dnN6ZzgifQ.hEw3XgEFt4h7msJ9C4mgAQ)`;
+  canvas.style.backgroundImage = `url(https://api.mapbox.com/styles/v1/mapbox/${mapMode}/static/${mapLagitute},${mapLongitude},${mapZoom},0,${mapAngle}/${canvasWidth}x${canvasHeight}?access_token=pk.eyJ1IjoibWFyaS1hbm5lIiwiYSI6ImNsMG83amw3bTFuaTQzaWw0Z2w1dnN6ZzgifQ.hEw3XgEFt4h7msJ9C4mgAQ)`;
 };
+
+settingUpCanvas()
